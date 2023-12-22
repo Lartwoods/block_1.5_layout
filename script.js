1123632b93312e7976ce839329435de5728c6bb7
vladaarta <lartwoods@yandex.ru>
2023-12-23 02:31:13 +0300

    test

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
+    slidesPerView: 1.2,
+    spaceBetween:20,
+    // freeMode:true,
   
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
     },
   
     // Navigation arrows
