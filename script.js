dd5ef3e12c9bc28f896ef073ea9253e4a94d63e7
vladaarta <lartwoods@yandex.ru>
2023-12-24 17:43:28 +0300

    swiper done

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 1.2,
-    spaceBetween:20,
-    // freeMode:true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-   
-  });
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  // slidesPerView: 1.2,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
